83ee8a96453c5930bb150312514dca58100fcb35
Michael Bao <halo343gs@gmail.com>
2017-11-18 22:02:05 -0500

    hmm :thinking:

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,6 +6,7 @@ if [[ ${TRAVIS_OS_NAME} == "linux" ]]; then
     sudo apt-get -qq update
     sudo apt-get install -y libopenimageio1.3
     sudo apt-get install -y libopenimageio-dev
+    dpkg -L libopenimageio-dev
 elif [[ ${TRAVIS_OS_NAME} == "osx" ]]; then
     brew update
     brew install homebrew/science/openimageio
