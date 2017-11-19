235afc4232c92acb1065294e6badd6de1647e451
Michael Bao <halo343gs@gmail.com>
2017-11-18 23:48:04 -0500

    include proper header for std::runtime_error

--- cpp/lib/image.h
+++ cpp/lib/image.h
@@ -3,9 +3,9 @@
 // found in the LICENSE file. The SIFT algorithm is
 // patented and its use for commercial applications must be licensed.
 // See the LICENSE file for details.
-#include <exception>
 #include <sstream>
 #include <string>
+#include <stdexcept>
 #include <vector>
 
 namespace sift
